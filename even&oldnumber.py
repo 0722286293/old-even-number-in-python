4df08b703d253e87b8e39d85643b98da788f4f04
risika <64900522+0722286293@users.noreply.github.com>
2021-02-11 13:15:28 +0300

    first commit

---
+++ even&oldnumber.py
@@ -0,0 +1,10 @@
+x=100
+if x%2==0:
+    print("the  number is even number")
+else:
+    print("the number is old number")
+x=33
+if x&1==1:
+    print("the number is old number")
+else:
+    print("the number is even number")
